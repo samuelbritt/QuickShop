2a559e0ea9a7b9ba8b56e039b37aa2c64309e099
Kunal Malhotra <kmalhotra7@gatech.edu>
2012-12-01 15:18:58 -0500

    Irrelevant code removed and cleaned up

--- src/com/example/quickshop/TestExpand.java
+++ src/com/example/quickshop/TestExpand.java
@@ -1,19 +1,11 @@
 package com.example.quickshop;
 
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Hashtable;
-import java.util.Iterator;
 import java.util.List;
-import java.util.Set;
-
-
-
 import android.os.Bundle;
 import android.app.Activity;
 import android.content.Intent;
 import android.text.Editable;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -24,32 +16,21 @@ import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ExpandableListView;
 import android.widget.Spinner;
-import android.widget.Toast;
 import android.support.v4.app.NavUtils;
 import com.example.quickshop.R;
 
 public class TestExpand extends Activity implements OnItemSelectedListener {
-	private String[] catList;
 	List<String> Cat = new ArrayList<String>();
 	public final static String EXTRA_MESSAGE = "com.example.quickshop.MESSAGE";
 	String message;
 	
 	private ExpandListAdapter ExpAdapter;
 	private ArrayList<ExpandListGroup> ExpListItems;
-	private ArrayList<ExpandListGroup> ExpListItems_new;
 	private ExpandableListView ExpandList;
 	Spinner spinner;
 	private String itemChild;
     private String dropDownCat;
-    private String flag = "false";
-  
-    //ArrayList<ExpandListChild> list2 = new ArrayList<ExpandListChild>();
-    
-    private ArrayList<ExpandListGroup> ExpListChild;
-    //Hashtable itemCatHash = new Hashtable();
-    
-    HashMap itemCatMap = new HashMap();
-    ArrayList itemCatList = new ArrayList();
+    private String addItemButtonClickflag = "false";
     
 	@Override
     public void onCreate(Bundle savedInstanceState) {
@@ -59,65 +40,26 @@ public class TestExpand extends Activity implements OnItemSelectedListener {
         String noDrop = "";
         
         // INSERT STATEMENTS go here for the first time . You need to remove them after inserting to avoid Key constraint. 
-      
-        
-        
-        
-        // DatabaseHandler db = new DatabaseHandler(this);
-       
-       //inserting stores in the store table 
-      // Log.d("Insert: ","Inserting ..");
-       //db.addItemCatNew(new ItemCatNew("Bread2", "Breads"));
-        
-     //  List<Store> stores = (List<Store>) db.getStore(1);
-       
-       /*
-       db.deleteItems();
-        Log.d("Reading: ", "Reading..");
-        //ItemCatNew itc =  (ItemCatNew) db.getItemsNew("Breads");
-        List<ItemCatNew> itcList = (List<ItemCatNew>) db.getItemsNewList("Wine Coolers");
-        
-        for(ItemCatNew itc : itcList){
-
-            String log = "Cat name: " + itc.getCatName() +  " itemName: " + itc.getItemName();
-             	Log.d("Name: ", log);
-             
-        }
-       
-       */
-        /*
-        DatabaseHandler db = new DatabaseHandler(this);
-    	db.onCreate(null);
-    	Log.d("Insert:", "Inserting..");
-    	db.addItemCatNew(new ItemCatNew("Kunal", "Malhotra"));
-    	
-    	Log.d("Reading", "Reading cats");
-    	List<ItemCatNew> items = (List<ItemCatNew>) db.getItemsNew("Malhotra");
-    	
-    	for(ItemCatNew ic : items){
-    		String log = "ItemName " + ic.getItemName() + ", ItemCat " + ic.getCatName();
-    		Log.d("Name: ", log);
-    	}
-    	*/
         
         ExpandList = (ExpandableListView) findViewById(R.id.ExpList);
-        ExpListItems = SetStandardGroups(noChild, noDrop, flag);
+        ExpListItems = SetStandardGroups(noChild, noDrop, addItemButtonClickflag);
         ExpAdapter = new ExpandListAdapter(TestExpand.this, ExpListItems);
         ExpandList.setAdapter(ExpAdapter);
         
         Intent intent = getIntent();
         message = intent.getStringExtra(TestExpand.EXTRA_MESSAGE);
-      //  itemChild = message;
         // Loading spinner data
         spinner = (Spinner) findViewById(R.id.spinner);
-        
+       
         spinner.setOnItemSelectedListener(this);
         
         loadSpinnerData();
         
     }
  
-	
+
+	/** 
+	 * LOADING THE DROP DOWN LIST */
 	@SuppressWarnings("null")
 	private void loadSpinnerData() {
 	
@@ -126,12 +68,12 @@ public class TestExpand extends Activity implements OnItemSelectedListener {
 		
 		ArrayList<String> catStringData = new ArrayList<String>();
 		
-		for(Category c : catData){
-			String var = c.getCatName();
-			catStringData.add(var);
-			//Log.d("Name",var);
-		}
-		
+			for(Category c : catData){
+				
+				String var = c.getCatName();
+				catStringData.add(var);
+				}
+			
 		
 		ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this,
                 android.R.layout.simple_spinner_item, catStringData);
@@ -164,165 +106,52 @@ public class TestExpand extends Activity implements OnItemSelectedListener {
         return super.onOptionsItemSelected(item);
     }
    
-    /*
-    public ArrayList<ExpandListGroup> SetStandardGroups() {
+   
+    public ArrayList<ExpandListGroup> SetStandardGroups(String ch , String drop, String btnClickflag) {
     	
-			
-	    	 DatabaseHandler db = new DatabaseHandler(this);
-	    	 List<Category> categories = db.getAllCategories();
-	         
-	         for(Category c : categories){
-	        	 
-	        	 ExpandListGroup gru1 = new ExpandListGroup();
-	            	 String var = c.getCatName();
-	            	 gru1.setName(var);
-	            	 
-	            	 if(var.equals(dropDownCat) && flag.equals("true")){
-	            	//if(var.equals("Wine")){	
-	            	 Toast.makeText(getApplicationContext(), flag, Toast.LENGTH_LONG).show();
-	            		 ExpandListChild child = new ExpandListChild();
-		            	 child.setName("itemChildxxx");
-		            	 list2.add(child);
-		            	 gru1.setItems(list2);
-	            	 }
-	            	 
-	            	 list.add(gru1);
-	            	 
-	        	 }
-	        	 
-	
-		return list;
-    	 
-    }
-    
-    */
-    public ArrayList<ExpandListGroup> SetStandardGroups(String ch , String drop, String flag) {
-    	
-    	ArrayList<ExpandListGroup> list = new ArrayList<ExpandListGroup>();
+    	ArrayList<ExpandListGroup> groupList = new ArrayList<ExpandListGroup>();
     	  
     	DatabaseHandler db = new DatabaseHandler(this);
     	List<Category> categories = db.getAllCategories();
     	
-    	// Add function for getItemCatNew
-    	
     	for(Category c: categories){
-    		ExpandListGroup gru1 = new ExpandListGroup();
-    		String var = c.getCatName();
-    		gru1.setName(var);
     		
-    		if(flag.equals("true")){
-    			//if(db.getItemsNew(var).getItemName().equals(null)){
-    				
-    		//	} 
-    			//else {
+    		ExpandListGroup group = new ExpandListGroup();
+    		String var = c.getCatName();
+    		group.setName(var);
+    		
+    		if(btnClickflag.equals("true")){
+    			
     				List<ItemCatNew> items = (List<ItemCatNew>) db.getItemsNewList(var);
-    	    		//if(var.equals(drop)){
-    	            ArrayList<ExpandListChild> list2 = new ArrayList<ExpandListChild>();
-    	        	for(ItemCatNew itc : items){
+    	    		ArrayList<ExpandListChild> childList = new ArrayList<ExpandListChild>();
+    	        	
+    	    		for(ItemCatNew itc : items){
     	        		ExpandListChild child = new ExpandListChild();
     	        		child.setName(itc.getItemName());
-    	        		list2.add(child);
+    	        		childList.add(child);
     	           	}
     	            	
-    	        	gru1.setItems(list2);
-    	    		//}
-    			//} // else end
-    	
-    		}
-        	list.add(gru1);
-    		
-    	}
-    
-    	return list;
-
-
-    }
-    // Make a generic method SetStandardGroups which takes into acc the dropdowncat and itemchild and ignores it when its called 
-    // from the onCreate Method. 
+    	        	group.setItems(childList);
+    	   		}
+        	groupList.add(group);
+      	}
    
-    /*
-    public ArrayList<ExpandListGroup> SetStandardGroups() {
+    	return groupList;
+    }
     
-    	ArrayList<ExpandListGroup> list = new ArrayList<ExpandListGroup>();
-    	ArrayList<ExpandListChild> list2 = new ArrayList<ExpandListChild>();
-        ExpandListGroup gru1 = new ExpandListGroup();
-        gru1.setName("Comedy");
-        ExpandListChild ch1_1 = new ExpandListChild();
-        ch1_1.setName("A movie");
-        ch1_1.setTag(null);
-        list2.add(ch1_1);
-        ExpandListChild ch1_2 = new ExpandListChild();
-        ch1_2.setName("An other movie");
-        ch1_2.setTag(null);
-        list2.add(ch1_2);
-        ExpandListChild ch1_3 = new ExpandListChild();
-        ch1_3.setName("And an other movie");
-        ch1_3.setTag(null);
-        list2.add(ch1_3);
-        gru1.setItems(list2);
-        list2 = new ArrayList<ExpandListChild>();
-        
-        ExpandListGroup gru2 = new ExpandListGroup();
-        gru2.setName("Action");
-        ExpandListChild ch2_1 = new ExpandListChild();
-        ch2_1.setName("A movie");
-        ch2_1.setTag(null);
-        list2.add(ch2_1);
-        ExpandListChild ch2_2 = new ExpandListChild();
-        ch2_2.setName("An other movie");
-        ch2_2.setTag(null);
-        list2.add(ch2_2);
-        ExpandListChild ch2_3 = new ExpandListChild();
-        ch2_3.setName("And an other movie");
-        ch2_3.setTag(null);
-        list2.add(ch2_3);
-        gru2.setItems(list2);
-        list.add(gru1);
-        list.add(gru2);
-        
-        return list;
-        }
-*/
-    public void button1 (View view){
+    public void btnAddItem (View view){
     	
     	EditText editText = (EditText) findViewById(R.id.editText2);
     	Editable temp = editText.getText();
     	itemChild = temp.toString();
-    	Toast.makeText(getApplicationContext(), itemChild, Toast.LENGTH_LONG).show();
-    	flag = "true";
-    	
-    // Add Insert statements
+    	addItemButtonClickflag = "true";
+   
     	DatabaseHandler db = new DatabaseHandler(this);
     	db.addItemCatNew(new ItemCatNew(itemChild, dropDownCat));
-    	// ExpListItems = SetStandardGroups(itemChild , dropDownCat) ------------ add these 2 parameters in the SetStandardGroups 
     	
-    	//itemCatList.add("good");
-    	//itemCatList.add("bad");
-    	//itemCatList.add(itemChild);
-    	
-    	//itemCatMap.put(dropDownCat, itemCatList);
-    	//ArrayList l1 = (ArrayList) itemCatMap.get(dropDownCat);
-    	//Iterator iter = l1.iterator();
-    	//ArrayList L = (ArrayList) itemCatMap.get(dropDownCat);
-    	//Iterator it = L.iterator();
-    	
-    
-    	//while(it.hasNext()){
-    		//Toast.makeText(getApplicationContext(),"The value is" + it.next(), Toast.LENGTH_LONG).show();
-    	//}
-    	
-    	
-    	
-    	ExpListItems = SetStandardGroups(itemChild , dropDownCat, flag);
+    	ExpListItems = SetStandardGroups(itemChild , dropDownCat, addItemButtonClickflag);
     	ExpAdapter = new ExpandListAdapter(TestExpand.this, ExpListItems);
         ExpandList.setAdapter(ExpAdapter);
-    	
-    	//Intent intent = new Intent(this , TestExpand.class);
-    	//EditText editText = (EditText) findViewById(R.id.editText2);
-    	//String message = editText.getText().toString();
-    	//intent.putExtra(EXTRA_MESSAGE, message);
-    	//startActivity(intent);
-    	
     	
     }
 
@@ -332,8 +161,7 @@ public class TestExpand extends Activity implements OnItemSelectedListener {
 			long id) {
 		
 		dropDownCat = parent.getItemAtPosition(position).toString();
-		 Toast.makeText(parent.getContext(), "You selected: " + dropDownCat,
-	                Toast.LENGTH_LONG).show();
+		 
 	}
 
 
@@ -346,16 +174,4 @@ public class TestExpand extends Activity implements OnItemSelectedListener {
     
 }
 
-/* else { // the case when child and dropdown has been sent
-        		 ExpandListGroup grup1 = new ExpandListGroup();
-        		 String var = c.getCatName();
-        		 grup1.setName(var);
-        		 if(dropDownCat.equals(var)){
-        			 ArrayList<ExpandListChild> listChild = new ArrayList<ExpandListChild>();
-        			 ExpandListChild ch = new ExpandListChild();
-        			 ch.setName(itemChild);
-        			 listChild.add(ch);
-        			 grup1.setItems(listChild);
-        		 }
- * 
- */
+
