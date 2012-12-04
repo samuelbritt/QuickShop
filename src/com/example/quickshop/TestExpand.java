1414be617a161d49e4b75f16dc1ebd1bd410d823
Sam Britt <samuelbritt@gmail.com>
2012-12-04 09:45:30 -0500

    even better add item layout, change background color for distiguishing

--- src/com/example/quickshop/TestExpand.java
+++ src/com/example/quickshop/TestExpand.java
@@ -7,7 +7,6 @@ import android.os.Bundle;
 import android.app.Activity;
 import android.content.Intent;
 import android.database.SQLException;
-import android.text.Editable;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
